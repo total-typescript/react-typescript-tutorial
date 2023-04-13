5dc2a614c0760f6296607d757127e7a0e0ebf548
Matt Pocock <mattpocockvoice@gmail.com>
2023-04-13 08:40:17 +0100

    WIP

--- src/02-components/03-typing-components.solution.5.tsx
+++ src/02-components/03-typing-components.solution.5.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-type ButtonComponent = (props: { className: string }) => // TODO - fix this?
-JSX.Element;
+type ButtonComponent = (props: { className: string }) => JSX.Element;
 
 export const Button: ButtonComponent = ({ className }) => {
   return <button className={className}></button>;
