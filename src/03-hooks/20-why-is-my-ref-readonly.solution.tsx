f14ff7dca1137d2080c9f5c0ee6ca5b52b2507ca
Matt Pocock <mattpocockvoice@gmail.com>
2023-04-24 16:21:53 +0100

    Changed 20 solution

--- src/03-hooks/20-why-is-my-ref-readonly.solution.tsx
+++ src/03-hooks/20-why-is-my-ref-readonly.solution.tsx
@@ -1,9 +1,15 @@
 import { useRef } from "react";
 
 export const Component = () => {
-  const ref = useRef<string>();
+  const firstOverload = useRef<string>("124123123");
 
-  ref.current = "Hello";
+  firstOverload.current = "123j12jhb123jhb";
+
+  const secondOverload = useRef<string>(null);
+
+  secondOverload.current = "Hello";
+
+  const thirdOverload = useRef<string>();
 
   return null;
 };
