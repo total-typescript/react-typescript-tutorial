bd5987dbc2fe5de24af05c2a2f27542265bec208
apo1798 <nploxz@gmail.com>
2023-05-05 23:30:20 +0800

    change into onClick prop name in exercise 07

--- src/02-components/07-typing-onclick-handlers.problem.tsx
+++ src/02-components/07-typing-onclick-handlers.problem.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
 interface ButtonProps {
   className: string;
   children: React.ReactNode;
 }
 
-export const Button = ({ children, className, onChange }: ButtonProps) => {
+export const Button = ({ children, className, onClick }: ButtonProps) => {
   return (
-    <button onChange={onChange} className={className}>
+    <button onClick={onClick} className={className}>
       {children}
     </button>
   );
