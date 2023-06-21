384fa20edd572fb7a90940d4a6e5d7efeb8484c9
Matt Pocock <mattpocockvoice@gmail.com>
2023-06-21 11:06:45 +0100

    Fixed 27 solution

--- src/04-advanced-props/27-either-all-these-props-or-none.solution.tsx
+++ src/04-advanced-props/27-either-all-these-props-or-none.solution.tsx
@@ -5,7 +5,10 @@ type InputProps = (
       value: string;
       onChange: ChangeEventHandler;
     }
-  | {}
+  | {
+      value?: undefined;
+      onChange?: undefined;
+    }
 ) & {
   label: string;
 };
