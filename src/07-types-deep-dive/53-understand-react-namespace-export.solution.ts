47582330c72bb6c717a721535a8471998d3472d2
Matt Pocock <mattpocockvoice@gmail.com>
2023-07-17 14:57:23 +0100

    Fixed 53

--- src/07-types-deep-dive/53-understand-react-namespace-export.solution.ts
+++ src/07-types-deep-dive/53-understand-react-namespace-export.solution.ts
@@ -1,15 +1,15 @@
+import React from "react";
+
 /**
  * Questions to answer:
  */
 
 // 1. What is the React namespace?
-// 1a. Why can it be accessed globally as a type?
 type Example = React.ReactNode;
 //             ^?
 
 /**
- * 2a. Most namespaces can't be used as values. So how come
- * we can use React as a value here?
+ * 2. How come React can be used BOTH as a value and a type?
  *
  * HINT - we're adding LOTS of things to React's namespace in
  * later exercises, so make sure when you go-to-definition you
