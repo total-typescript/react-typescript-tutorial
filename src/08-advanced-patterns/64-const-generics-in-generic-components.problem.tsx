10c28d6d82b5dafbc2cc8bdd19034571d25d1e49
Matt Pocock <mattpocockvoice@gmail.com>
2023-07-24 15:43:50 +0100

    Removed const generics

--- src/08-advanced-patterns/64-const-generics-in-generic-components.problem.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * Have a Form component that can receive an object where
- * the keys are the different form elements, and have the
- * values inferred based on values passed in.
- */
-
-const Form = <const TValues extends Record<string, any>>() => {};
