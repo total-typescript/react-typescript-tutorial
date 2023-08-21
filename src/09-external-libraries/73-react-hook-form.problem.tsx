21047abaf0350f00e673e2d361f6624e3f030e25
Matt Pocock <mattpocockvoice@gmail.com>
2023-08-21 12:56:04 +0100

    Changed to P/S

---
+++ src/09-external-libraries/73-react-hook-form.problem.tsx
@@ -0,0 +1,52 @@
+import { FieldValues, useForm } from "react-hook-form";
+import { Equal, Expect } from "../helpers/type-utils";
+
+/**
+ * 1. When you provide default values to useForm, the return type of getValues
+ * gets inferred as the shape of those values.
+ *
+ * Investigate why this is, and what TFieldValues is being used for.
+ */
+const Example1 = () => {
+  const form = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+
+  return (
+    <form
+      onSubmit={form.handleSubmit((values) => {
+        type test = Expect<
+          Equal<typeof values, { firstName: string; lastName: string }>
+        >;
+      })}
+    >
+      <input {...form.register("firstName")} />
+      <input {...form.register("lastName")} />
+    </form>
+  );
+};
+
+/**
+ * 2. When you don't pass a default value, the return type of getValues is
+ * inferred as FieldValues.
+ *
+ * Investigate why this is, and what type FieldValues is.
+ */
+
+const Example2 = () => {
+  const form = useForm();
+
+  return (
+    <form
+      onSubmit={form.handleSubmit((values) => {
+        type test = Expect<Equal<typeof values, FieldValues>>;
+      })}
+    >
+      <input {...form.register("firstName")} />
+      <input {...form.register("lastName")} />
+    </form>
+  );
+};
--- src/09-external-libraries/73-react-hook-form.solution.tsx
+++ src/09-external-libraries/73-react-hook-form.solution.tsx
@@ -1,15 +1,9 @@
 import { FieldValues, useForm } from "react-hook-form";
 import { Equal, Expect } from "../helpers/type-utils";
 
-/**
- * 1. When you provide default values to useForm, the return type of getValues
- * gets inferred as the shape of those values.
- *
- * Investigate why this is, and what TFieldValues is being used for.
- */
 const Example1 = () => {
   const form = useForm({
-    defaultValues: {
+    values: {
       firstName: "",
       lastName: "",
     },
@@ -29,15 +23,8 @@ const Example1 = () => {
   );
 };
 
-/**
- * 2. When you don't pass a default value, the return type of getValues is
- * inferred as FieldValues.
- *
- * Investigate why this is, and what type FieldValues is.
- */
-
 const Example2 = () => {
-  const form = useForm({});
+  const form = useForm();
 
   return (
     <form
